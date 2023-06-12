4f78f011b4170bb9ff6518df15747bfd1b35e590
Campbell Forsyth <camsh69@gmail.com>
2023-06-12 18:49:58 +0100

    updates

--- utilities/LogUtil.py
+++ utilities/LogUtil.py
@@ -1,9 +1,10 @@
+import os
 import logging
 import time
 
 
 class Logger:
-    def __init__(self, logger, file_level=logging.INFO):
+    def __init__(self, logger, file_level=logging.INFO, log_directory="./logs"):
         self.logger = logging.getLogger(logger)
         self.logger.setLevel(logging.DEBUG)
 
@@ -12,9 +13,11 @@ class Logger:
         )
 
         curr_time = time.strftime("%Y-%m-%d")
-        self.LogFileName = "log" + curr_time + ".txt"
-        # "a" to append the logs in same file, "w" to generate new logs and delete old one
-        fh = logging.FileHandler(self.LogFileName, mode="a")
+        log_file_name = f"log{curr_time}.txt"
+        log_file_path = os.path.join(log_directory, log_file_name)
+
+        # "a" to append the logs in the same file, "w" to generate new logs and delete the old one
+        fh = logging.FileHandler(log_file_path, mode="a")
         fh.setFormatter(fmt)
         fh.setLevel(file_level)
         self.logger.addHandler(fh)
